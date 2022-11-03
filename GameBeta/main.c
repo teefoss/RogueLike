0de568f3181708384073d55129f4a136f76975b0
xiphiasnonus <foster.pianist@gmail.com>
2022-11-02 23:17:24 -0400

    Initial Commit

---
+++ GameBeta/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GameBeta
+//
+//  Created by Thomas Foster on 11/2/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
